4ed917f674f4bd1f09d55fb89da0ee9ac06429fe
adiyaddula <adithyayaddula.gmail.com>
2023-05-09 09:16:03 +0000

    mul.sh

---
+++ scripts/art.sh
@@ -0,0 +1,8 @@
+a=12
+b=13
+if [$a -gt $b]
+then
+	echo "ofcourse $a"
+else
+	echo "ofcourse $b"
+fi
---
+++ scripts/mul.sh
@@ -0,0 +1,16 @@
+
+
+a=4
+b=2
+c=3
+
+if [ $a -ge $b && $a -ge $c];then
+	echo "its $a"
+elif
+	[ $b -ge $a && $b -ge $c];then
+	echo "its $b"
+else
+	echo "its $c"
+fi
+
+	
